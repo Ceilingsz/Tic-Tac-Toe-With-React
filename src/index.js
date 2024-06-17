10e2a5d375fc8034c5b36da076986fc0840b33e0
Chaitanya Nemmani <chaitanyanemmani@gmail.com>
2024-06-17 19:35:43 +0530

    Added X/O based on turns

--- src/index.js
+++ src/index.js
@@ -1,33 +1,69 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {useState} from 'react'
 import "./styles.css"
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-function Square(props) {
-  return (
-    <button className='Square' onClick={props.onTlick}>{props.value === 1 ? 'X' : 'O'}</button>
-  )
+function Empty(){
+  return <div></div>
 }
 
+function Cross(){
+  return <div>X</div>
+}
+
+function Oh(){
+  return <div>O</div>
+}
+
+function Display(props) {
+  if(props.val === 0)
+    return <Empty />
+  if(props.val === 1)
+    return <Cross />
+  if(props.val === 2)
+    return <Oh />
+}
+
+
+function Square(props) {
+  const [value, setValue ] = useState(0);
+  function handleClick() { 
+    if(props.value === 1)
+    setValue(1);  
+    else
+    setValue(2)
+    props.updateTurn(() => {
+      if(props.value === 1)
+        return 0;
+      else
+        return 1;
+    })
+  }
+
+  return (
+    <button className='Square' onClick = {handleClick} disabled={value === 0 ? false : true}><Display  val={value}/></button>
+  )
+} 
+
 function BoardRow(props) {
-  const [Array, func] = React.useState([0,0,0]);
-  console.log(Array);
+  
   return (
     <div className='Board-Row'>
-      <Square value = {Array[0]} onTlick = {() => {func([1, Array[1], Array[2]])}}/>
-      <Square value = {Array[1]} onTlick = {() => {func([Array[0], 1, Array[2]])}}/>
-      <Square value = {Array[2]} onTlick = {() => {func([Array[0], Array[1], 1])}}/>
+      <Square updateTurn = {props.updateTurn} value = {props.value}/>
+      <Square updateTurn = {props.updateTurn} value = {props.value}/>
+      <Square updateTurn = {props.updateTurn} value = {props.value}/>
     </div>
   )
 }
 
 function Board(){
-
+  const [turn, setTurn] = useState(1);
   return (
   <div className='Board'>
-    <BoardRow row = {1} />
-    <BoardRow row = {4} />
-    <BoardRow row = {7} />
+    <BoardRow updateTurn = {setTurn} value = {turn}/>
+    <BoardRow updateTurn = {setTurn} value = {turn}/>
+    <BoardRow updateTurn = {setTurn} value = {turn}/>
   </div>
   )
 }
